f17feb6585f9586f8c49f2738a67e5ab8e04959e
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-12-07 13:47:54 +0100

    wijzigingen

--- src/MyGroceriesApp.jsx
+++ src/MyGroceriesApp.jsx
@@ -1,16 +1,14 @@
-import './mygroceries.css';
-import React,{Component} from 'react'
-import List from './components/List'
+import React from "react";
+import List from "./components/List";
 
-class MyGroceriesApp extends Component {
-
-
-  render(){ return (<div>
-                       <List /> 
-                    </div>)
-     
-          }
-             
+class MyGroceriesApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <List />
+      </div>
+    );
+  }
 }
 
 export default MyGroceriesApp;
